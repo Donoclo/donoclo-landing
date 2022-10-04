f948efdf187b8173ab061de0801bde4f70b9b16f
micko6420 <micko6420@gmail.com>
2022-10-04 23:59:03 +0900

    closet image 추가

---
+++ src/component/Closet/index.js
@@ -0,0 +1,13 @@
+import "./index.css";
+
+const Closet = () => {
+  return(
+    <>
+      <div className="closet-container">
+        hello there
+      </div>
+    </>
+  )
+}
+
+export default Closet;
